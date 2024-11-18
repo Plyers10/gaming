7a0c863646c7e9536cd618119352934bed635d8f
Plyers10 <evanoliverc25@gmail.com>
2024-11-18 12:31:46 -0800

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[][] rr = {{1,2,3,4},{5,6,7,8}};
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+
+}
